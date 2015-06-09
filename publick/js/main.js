6cc3cdbf576040bc2698dbc9a496c688b94065da
Oleg Rovensky <RovenskyOleg@i.ua>
2015-06-10 01:06:19 +0300

    Implement ajax call data_gonfig

--- publick/js/main.js
+++ publick/js/main.js
@@ -1,84 +1,54 @@
 "use strict";
 
 window.onload = function() {
-        var el = document.getElementById('slideshow1'),
-            config = {
-                images: [
-                    '../publick/img/1.png',
-                    '../publick/img/2.png',
-                    '../publick/img/3.png',
-                    '../publick/img/4.png'
-                ],
-                mode: 'auto',
-                swipeSpeed: 500,
-                swipeDelay: 1500
+    var facade = new Facade();
+    var self = this;
+    var cachedData;
+    var uri = 'publick/data/config_data.json';
+
+    facade.getResult("GET", uri, mockCallback);
+
+    function mockCallback(data) {
+        cachedData = JSON.parse(data);
+        self.init();
+    };
+
+    this.init = function () {
+        var slider = {
+                baseSlider: new BaseSlider(cachedData.config),
+                slider: new Slider(cachedData.configAuto),
+                fadein: new FadeIn(cachedData.configAuto)
             },
-            configAuto = {
-                images: [
-                    '../publick/img/1.png',
-                    '../publick/img/2.png',
-                    '../publick/img/3.png',
-                    '../publick/img/4.png'
-                ],
-                mode: 'auto',
-                swipeSpeed: 500,
-                swipeDelay: 2000
+            slider_manual = {
+                baseSlider: new BaseSlider(cachedData.configManual),
+                slider: new Slider(cachedData.configManual),
+                fadein: new FadeIn(cachedData.configManual)
             },
-            configManual = {
-                images: [
-                    '../publick/img/1.png',
-                    '../publick/img/2.png',
-                    '../publick/img/3.png',
-                    '../publick/img/4.png'
-                ],
-                mode: 'manual',
-                swipeSpeed: 500,
-                swipeDelay: 2000
-            },
-            configAutomanual = {
-                images: [
-                    '../publick/img/1.png',
-                    '../publick/img/2.png',
-                    '../publick/img/3.png',
-                    '../publick/img/4.png'
-                ],
-                mode: 'automanual',
-                swipeSpeed: 500,
-                swipeDelay: 2000
+            slider_automanual = {
+                baseSlider: new BaseSlider(cachedData.configAutomanual),
+                slider: new Slider(cachedData.configAutomanual),
+                fadein: new FadeIn(cachedData.configAutomanual)
             };
+        
+        // auto
 
-    // auto
-    var slider = {
-        baseSlider: new BaseSlider(config),
-        slider: new Slider(configAuto),
-        fadein: new FadeIn(configAuto)
-    };
-    
-    slider.baseSlider.init(document.getElementById('slideshow1'), document.getElementById('left1'),document.getElementById('right1'));
-    slider.slider.init(document.getElementById('slideshow2'), document.getElementById('left2'),document.getElementById('right2'));
-    slider.fadein.init(document.getElementById('slideshow3'), document.getElementById('left3'),document.getElementById('right3'));
+        slider.baseSlider.init(document.getElementById('slideshow1'), document.getElementById('left1'),document.getElementById('right1'));
+        slider.slider.init(document.getElementById('slideshow2'), document.getElementById('left2'),document.getElementById('right2'));
+        slider.fadein.init(document.getElementById('slideshow3'), document.getElementById('left3'),document.getElementById('right3'));
 
-    // manual
-    var slider_manual = {
-        baseSlider: new BaseSlider(configManual),
-        slider: new Slider(configManual),
-        fadein: new FadeIn(configManual)
-    };
+        // manual
+        
+        slider_manual.baseSlider.init(document.getElementById('slideshow4'), document.getElementById('left4'),document.getElementById('right4'));
+        slider_manual.slider.init(document.getElementById('slideshow5'),document.getElementById('left5'),document.getElementById('right5'));
+        slider_manual.fadein.init(document.getElementById('slideshow6'),document.getElementById('left6'),document.getElementById('right6'));
 
-    slider_manual.baseSlider.init(document.getElementById('slideshow4'), document.getElementById('left4'),document.getElementById('right4'));
-    slider_manual.slider.init(document.getElementById('slideshow5'),document.getElementById('left5'),document.getElementById('right5'));
-    slider_manual.fadein.init(document.getElementById('slideshow6'),document.getElementById('left6'),document.getElementById('right6'));
+        // automanual
 
-    // automanual
-    var slider_automanual = {
-        baseSlider: new BaseSlider(configAutomanual),
-        slider: new Slider(configAutomanual),
-        fadein: new FadeIn(configAutomanual)
-    };
-    
-    slider_automanual.baseSlider.init(document.getElementById('slideshow7'), document.getElementById('left7'),document.getElementById('right7'));
-    slider_automanual.slider.init(document.getElementById('slideshow8'), document.getElementById('left8'),document.getElementById('right8'));
-    slider_automanual.fadein.init(document.getElementById('slideshow9'), document.getElementById('left9'),document.getElementById('right9'));
+        slider_automanual.baseSlider.init(document.getElementById('slideshow7'), document.getElementById('left7'),document.getElementById('right7'));
+        slider_automanual.slider.init(document.getElementById('slideshow8'), document.getElementById('left8'),document.getElementById('right8'));
+        slider_automanual.fadein.init(document.getElementById('slideshow9'), document.getElementById('left9'),document.getElementById('right9'));
+
+    }
 
     return this    
 }
